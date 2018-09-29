2448526cd3d643d2807c2a3de9a114cf13d91eda
FrancoCalle <francocalle93@gmail.com>
2018-09-29 14:59:38 -0400

    updated txt2csv to consider types of interaction

--- report/txt2csv.py
+++ report/txt2csv.py
@@ -1,40 +1,104 @@
-import os
-import csv
-import json
-import csv
-import re
-import sys
 import pandas as pd
+import numpy as np
+import scipy, random, copy, h5py, pandas, math, csv, sys, os, pickle, re, json
+import matplotlib.pyplot as plt
+from itertools import combinations
+from sklearn import preprocessing
+from numpy.random import RandomState
+from scipy import ndimage
+from scipy import stats
+from scipy import ndimage
+path = os.getcwd()
 
-def getBaseDirectory():
-    try:
-        return os.path.dirname(__file__)
-    except NameError:
-        return ''
+sub_routines = os.path.join(path,'..','QueVasaEstudiar','python', 'sub_routines')
+sys.path.append(sub_routines)
+from neural_predictor import neural_predictor
+from Import_MenuParameters import Import_MenuParameters
 
-interactions_path  = "C:/Users/Franco/GitHub/QueVasaEstudiar"
+#1. Import data and parameters:
+root = "C:/Users/Franco/GitHub" #Franco's MSI
 
-with open(interactions_path + '/interactions1.txt', 'r') as in_file:
+csv_path = root + "/Bots_Colombia/data/"
+
+def loadOutcomes():
+    outcomes = {}
+#    with open('python/Options_Outcomes_11_29_2017.csv') as csvfile:
+    with open(root + '/QueVasaEstudiar/python/Options_Outcomes_05_15_2018.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            outcomes[int(row['Option_ID'])] = row
+    return outcomes
+
+outcomes = loadOutcomes()
+
+def arange_outcomes(outcomes):
+
+    HEOptions = {
+    'MajorName'       : np.array([outcomes[x]['Option_Major_Name'] for x in outcomes.keys()]),
+    'OptionID'        : np.array([x for x in outcomes.keys()]).astype(int),
+    'InstCode'        : np.array([outcomes[x]['Option_Institution_Code'] for x in outcomes.keys()]).astype(int) -1,
+    'Cutoff'          : np.array([outcomes[x]['LowerBoundScore'] for x in outcomes.keys()]).astype(float),
+    'NoSearch'        : np.array([outcomes[x]['NoSearch'] for x in outcomes.keys()]).astype(int),
+    'LevelCode'       : np.array([outcomes[x]['Option_Level_Code'] for x in outcomes.keys()]).astype(int) -1,
+    'AreaCode'        : np.array([outcomes[x]['Option_Area_Code'] for x in outcomes.keys()]).astype(int) -1,
+    'LocationCode'    : np.array([outcomes[x]['Option_Location_Code'] for x in outcomes.keys()]).astype(int) -1,
+    'MajorCode'       : np.array([outcomes[x]['Option_Major_Code'] for x in outcomes.keys()]).astype(int) -1,
+    'AverageEarning'  : np.array([outcomes[x]['Outcome_IncomeAve'] if outcomes[x]['Outcome_IncomeAve'] != "" else "-999" for x in outcomes.keys()]).astype(float)
+    }
+
+    HEOptions['AreaID']     = np.array(list(set(HEOptions['AreaCode'])))
+    HEOptions['InstID']     = np.array(list(set(HEOptions['InstCode'])))      #We have to change this part
+    HEOptions['LevelID']    = np.array(list(set(HEOptions['LevelCode'])))
+    HEOptions['LocationID'] = np.array(list(set(HEOptions['LocationCode'])))
+    HEOptions['MajorID']    = np.array(list(set(HEOptions['MajorCode'])))
+
+    #Fixes:
+    aux_InstID = np.array(range(HEOptions['InstID'].shape[0]))
+
+    for y in HEOptions:
+        HEOptions[y] = HEOptions[y][:,np.newaxis]
+
+    for i in range(HEOptions['InstID'].shape[0]):
+        HEOptions['InstCode'][HEOptions['InstCode'] == HEOptions['InstID'][i,0]] = aux_InstID[i]
+
+    HEOptions['RelevantInst']  = aux_InstID[:,np.newaxis]
+    HEOptions['normW']         = 10000000
+    HEOptions['AverageEarning'][HEOptions['AverageEarning'] == -999] = np.nan
+
+    return HEOptions
+
+#1. Set up and import data:
+#==========================
+
+#1.1. Import Data:
+HEOptions = arange_outcomes(outcomes)
+
+with open(root+'/QueVasaEstudiar/interactions.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split(";") for line in stripped if line)
-    with open(interactions_path + '/log.csv', 'w') as out_file:
+    with open(root+'/QueVasaEstudiar/log.csv', 'w') as out_file:
         writer = csv.writer(out_file)
         writer.writerows(lines)
 
-data = pd.read_csv(interactions_path + '/log.csv', error_bad_lines=False)
-largo=len(data)
+allinteractions = pd.read_csv(root+'/QueVasaEstudiar/log.csv', error_bad_lines=False)
+allinteractions = allinteractions[(allinteractions.user != 'default-user') & (allinteractions.user.str.len() == 20)].reset_index(drop=True)
+
+allinteractions["year"] = allinteractions.timestamp.str[0:4].astype(int)
+allinteractions["month"] = allinteractions.timestamp.str[5:7].astype(int)
+allinteractions["day"] = allinteractions.timestamp.str[8:10].astype(int)
+allinteractions["hour"] = allinteractions.timestamp.str[11:13].astype(int)
+allinteractions["minute"] = allinteractions.timestamp.str[14:16].astype(int)
+allinteractions["second"] = allinteractions.timestamp.str[17:19].astype(int)
+
+DF1 = pd.read_csv(root+'/QueVasaEstudiar/Student_Features_Fall2018.csv'.format(100))
+DF2 = pd.read_csv(root+'/Bots_Colombia/ModelEstimation/deeplearning_estimation/interactions_bot.csv'.format(100))
+interactions = DF1.merge(DF2, left_on = 'Student_ID', right_on = 'Student_ID', how = 'left')
+interactions = interactions.merge(Student_Features, left_on = 'Student_ID', right_on = 'Student_ID')
+interactions = interactions.merge(Student_Features, left_on = 'Student_ID', right_on = 'Student_ID')
 
 
-data["year"] = data.timestamp.str[0:4]
-data["month"] = data.timestamp.str[5:7]
-data["day"] = data.timestamp.str[8:10]
-data["hour"] = data.timestamp.str[11:13]
-data["minute"] = data.timestamp.str[14:16]
-data["second"] = data.timestamp.str[17:19]
+#=====================================================================================================
 
-data["month"]=pd.to_numeric(data.month)
-data["year"]=pd.to_numeric(data.year)
-data["day"]=pd.to_numeric(data.day)
 
 after18 = data[(data.year == 2018) & (data.month >= 9) & (data.day >= 18)]
 
@@ -68,9 +132,22 @@ print(result["day_group"].value_counts())
 print("/nDesglose por ubicación: ")
 print(result["Student_Location_Name"].value_counts())
 
+#Type of interaction:
+A = allinteractions.event_name.value_counts()
+askAboutProgram   = A[0]
+initialQuestion   = A[1]
+askInstitution    = A[2]
+truth             = A[3]
+askCareer         = A[4]
+OPTIONS           = A[5]
+askArea           = A[6]
+OPTIONS_SELECTION = A[7]
+askLevel          = A[8]
 
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
 
-
+ax = sns.lineplot(x="day", y="second",hue="event", data=allinteractions)
 
 #Change report in spreadsheets:
 #=============================
